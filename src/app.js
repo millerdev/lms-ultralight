639c6726df41085ab914fd76d165759fe57553d8
Daniel Miller <dmiller@dimagi.com>
2016-12-23 14:37:13 -0500

    Add semantic-ui-react

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import state from './state';
 import Player from './player';
 
+import 'semantic-ui-css/semantic.min.css';
+
 const App = props => (
   <Player />
 );
--- src/player.js
+++ src/player.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+let playing = false;
+
+function togglePlayPause() {
+  playing = !playing;
+}
+
+const IconToggleButton = props => {
+  return (<Button
+    onClick={props.handleClick}
+    icon={ props.isOn() ? props.iconOff : props.iconOn }
+    />);
+};
 
 export default props => (
   <div>
-    player
+    <Button.Group basic size="small">
+      <Button icon="backward" />
+      <IconToggleButton
+        isOn={() => playing}
+        handleClick={togglePlayPause}
+        iconOn="play"
+        iconOff="pause" />
+      <Button icon="forward" />
+    </Button.Group>
+    <Button.Group basic size="small">
+      <Button icon="repeat" />
+      <Button icon="shuffle" />
+    </Button.Group>
+    
+    <Button icon="power" basic />
   </div>
 );
