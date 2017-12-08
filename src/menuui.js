46dd57a8721b111b0a80af97b7ff4e21f7abe60f
Daniel Miller <millerdev@gmail.com>
2017-12-08 15:48:05 -0500

    Fix scrolling on expand sidebar
    Expanding the sidebar is not as smooth as it was before, but that seems better than scrolling main view to the top each time the sidebar is opened.

--- src/menuui.js
+++ src/menuui.js
@@ -7,48 +7,46 @@ import './menu.styl'
 
 export const MainMenuUI = props => (
   <div className="mainmenu">
-    <PowerBar {...props} />
-    <Transition
-        visible={!!props.messages.error}
-        animation="slide down"
-        duration={500}
-        unmountOnHide>
-      <Message
-          className="messages"
-          onDismiss={props.onHideError}
-          onClick={props.onHideError}
-          size="small"
-          negative>
-        <Message.Content>
-          <Icon name="warning" size="large" />
-          {props.messages.error}
-        </Message.Content>
-      </Message>
-    </Transition>
-    <Sidebar.Pushable as="div" style={{ marginTop: "2.5em" }}>
-      <Sidebar
-          as={Menu}
-          animation="push"
-          width="wide"
-          visible={props.sidebarOpen}
-          borderless
-          style={{border: "none"}}
-          vertical>
-        <Menu.Item header name="ultralight" onClick={props.onToggleSidebar}>
-          <Icon name="close" /> Ultralight
-        </Menu.Item>
-        <Menu.Item name="search">
-          <MediaSearch {...props} />
-        </Menu.Item>
-      </Sidebar>
-      <Sidebar.Pusher>
-        <div className="ui padded grid">
-          <div className="sixteen wide column">
-            {props.children}
-          </div>
+    <Sidebar
+        as={Menu}
+        animation="push"
+        width="wide"
+        visible={props.sidebarOpen}
+        borderless
+        style={{border: "none"}}
+        vertical>
+      <Menu.Item header name="ultralight" onClick={props.onToggleSidebar}>
+        <Icon name="close" /> Ultralight
+      </Menu.Item>
+      <Menu.Item name="search">
+        <MediaSearch {...props} />
+      </Menu.Item>
+    </Sidebar>
+    <Sidebar.Pusher>
+      <PowerBar {...props} />
+      <Transition
+          visible={!!props.messages.error}
+          animation='slide down'
+          duration={500}
+          unmountOnHide>
+        <Message
+            className="messages"
+            onDismiss={props.onHideError}
+            onClick={props.onHideError}
+            size="small"
+            negative>
+          <Message.Content>
+            <Icon name="warning" size="large" />
+            {props.messages.error}
+          </Message.Content>
+        </Message>
+      </Transition>
+      <div className="ui padded grid">
+        <div className="sixteen wide column">
+          {props.children}
         </div>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+      </div>
+    </Sidebar.Pusher>
   </div>
 )
 
@@ -58,7 +56,11 @@ const PowerBar = props => {
   }
   const player = props.player.toObject()
   return (
-    <Menu size="small" fixed="top" borderless>
+    <Menu
+        attached={props.sidebarOpen && "top"}
+        fixed={props.sidebarOpen ? null : "top"}
+        size="small"
+        borderless>
       <Menu.Item fitted="vertically" onClick={props.onToggleSidebar}>
         <Icon name="content" size="large" />
       </Menu.Item>
