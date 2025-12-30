681a4dc2475775341233f2900ea039ec46c6d372
Daniel Miller <millerdev@gmail.com>
2025-12-30 16:16:59 -0500

    Upgrade to React 18

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -2,7 +2,7 @@ global.chai = require('chai')
 global.assert = chai.assert
 global.expect = chai.expect
 
-const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
+const Adapter = require('@cfaester/enzyme-adapter-react-18').default
 require('enzyme').configure({adapter: new Adapter()})
 
 chai.should()
