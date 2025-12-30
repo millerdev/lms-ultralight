ef36217fc41fbea8bd9f1ed51375910ca34f9924
Daniel Miller <millerdev@gmail.com>
2025-12-30 10:13:10 -0500

    Fix enzyme-adapter-react in tests with React 17
    Not sure how the tests were passing before, but after reinstalling node
    modules they failed with:
    
    Error: Cannot find module 'enzyme-adapter-react-16'

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -2,7 +2,7 @@ global.chai = require('chai')
 global.assert = chai.assert
 global.expect = chai.expect
 
-const Adapter = require('enzyme-adapter-react-16')
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17')
 require('enzyme').configure({adapter: new Adapter()})
 
 chai.should()
