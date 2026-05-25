786e506dd8f27aa78b58450c1381a4131f17f9b0
Daniel Miller <millerdev@gmail.com>
2026-05-25 15:52:00 -0400

    Add repo.xml release tooling

--- tools/pkg.js
+++ tools/pkg.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+const crypto = require("crypto")
 const FolderZip = require("folder-zip")
 const fs = require("fs-extra")
 
@@ -14,6 +15,20 @@ zip.zipFolder("UltralightSkin", {}, () => {
   }, () => {
     zip.file(installXmlPath, installXml.replace(/{version}/g, version))
     zip.writeToFileSync(outputPath)
+    const sha1 = crypto.createHash("sha1")
+      .update(fs.readFileSync(outputPath))
+      .digest("hex")
     console.log("package: " + outputPath)
+    console.log("sha1:    " + sha1)
+
+    const repoXmlPath = "repo.xml"
+    const releaseUrl = "https://github.com/millerdev/lms-ultralight/releases/download/v" + version + "/" + outputPath
+    const repoXml = fs.readFileSync(repoXmlPath, "utf8")
+    const updatedRepoXml = repoXml
+      .replace(/\bversion="[^"]*"(\s+minTarget)/, "version=\"" + version + "\"$1")
+      .replace(/\bsha="[^"]*"/, "sha=\"" + sha1 + "\"")
+      .replace(/\burl="[^"]*"/, "url=\"" + releaseUrl + "\"")
+    fs.writeFileSync(repoXmlPath, updatedRepoXml)
+    console.log("updated: " + repoXmlPath)
   })
 })
